8baf561da93427a170bee9f1b52c9869d04615da
Zach <muellerzr@gmail.com>
2019-11-13 11:04:05 -0600

    get_idxs

--- imports/object_detection/model.py
+++ imports/object_detection/model.py
@@ -1,6 +1,14 @@
 from torch import nn
 from .utils import *
+import numpy as np
 from fastai2.callback.hook import *
+
+def _get_sfs_idxs(sizes:Sizes) -> List[int]:
+    "Get the indexes of the layers where the size of the activation changes."
+    feature_szs = [size[-1] for size in sizes]
+    sfs_idxs = list(np.where(np.array(feature_szs[:-1]) != np.array(feature_szs[1:]))[0])
+    if feature_szs[0] != feature_szs[1]: sfs_idxs = [0] + sfs_idxs
+    return sfs_idxs
 
 class LateralUpsampleMerge(nn.Module):
     "Merge the features coming from the downsample path (in `hook`) with the upsample path."
