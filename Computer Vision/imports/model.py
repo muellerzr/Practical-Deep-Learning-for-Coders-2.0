2a98d65bbdd2f2ddc8f0aacf26c155b322ce82b8
Zachary Mueller <muellerzr@gmail.com>
2020-01-30 12:09:14 -0600

    conv2d

--- Computer Vision/imports/model.py
+++ Computer Vision/imports/model.py
@@ -1,7 +1,13 @@
 from torch import nn
 from .utils import *
-from fastai2.vision.models.unet import _get_sz_change_idxs
+from fastai2.vision.models.unet import _get_sz_change_idxs, hook_output
+from fastai2.layers import init_default
 from fastai2.callback.hook import model_sizes
+
+def conv2d(ni:int, nf:int, ks:int=3, stride:int=1, padding:int=None, bias=False, init=nn.init.kaiming_normal):
+    "Create and initialize `nn.Conv2d` layer."
+    if padding is None: padding = ks // 2
+    return init_default(nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, padding=padding, bias=bias, init)
 
 class LateralUpsampleMerge(nn.Module):
     "Merge the features coming from the downsample path (in `hook`) with the upsample path."
